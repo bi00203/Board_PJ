a59c85fbcb091e6411968854ef6fe242ed430592
bi00203 <bi00203@naver.com>
2022-12-29 04:19:33 +0900

    Initial commit

---
+++ src/main/java/com/oes/board_pj/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.oes.board_pj;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BoardPjApplication.class);
+    }
+
+}
